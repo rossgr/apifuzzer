1f4e1384593f746f36b0698a62efe808da456cb6
Ross Greenall <rossgreenall@gmail.com>
2023-10-01 01:10:41 +0100

    Added menuscreen, will develop further features and layout in future

--- main.py
+++ main.py
@@ -1,16 +1,29 @@
-# Requires an API wordlist, for ethical use only. 
+# To run:
+# $ cat YOURWORDLIST.txt | python main.py 
+
 import colorama
 from colorama import Fore, Style
 import requests
 import sys
 
 TARGET_URL = ""
-
+def menuScreen():
+    print('''  ______           __  __  __ __ __  __  __      
+/_____/\         /_/\/_/\/_//_//_/\/_/\/_/\     
+\::::_\/_  ______\:\ \:\ \:\\:\\:\ \:\ \:\ \    
+ \:\/___/\/______/\:\ \:\ \:\\:\\:\ \:\ \:\ \   
+  \:::._\/\__::::\/\:\ \:\ \:\\:\\:\ \:\ \:\ \  
+   \:\ \            \:\_\:\ \:\\:\\:\ \:\_\:\ \ 
+    \_\/             \_____\/\_______\/\_____\/ 
+                                                
+                                                
+Starting script - - -''')
+    
+menuScreen()
 
 for word in sys.stdin:
     res = requests.get(url=f'{TARGET_URL}/{word}');
     if res.status_code == 404:
-        print(Fore.RED + word)
         continue
     elif res.status_code <= 299 or res.status_code >=200:
         JSON_data = res.json()
